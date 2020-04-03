f8c94d78124c9196123ade6c33811ff11529f38e
“Patrick <pctennislessons@gmail.com>
2020-04-03 15:23:46 -0600

    tests failing

--- maxSubArray.test.js
+++ maxSubArray.test.js
@@ -1,9 +1,23 @@
 var assert = require('assert');
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
-  });
+describe('maxSumArray', function() {
+//   describe('#indexOf()', function() {
+//     it('should return -1 when the value is not present', function() {
+//       assert.equal([1, 2, 3].indexOf(4), -1);
+//     });
+//   });
+    describe('#subArrays', function(){
+        
+        it('returns an Array', function() {
+            assert.equal(Array.isArray(subArrays([1,2,-1])), true)
+        })
+
+        it('should return an array of subarrays', function(){
+            assert.equal(subArrays([1,2,-1]).length, 5)
+        })
+
+        it('The elements of #subArrays should be arrays', function(){
+            assert.equal(Array.isArray(subArrays([1,2,-1])[0]), true)
+        })
+    })
 });
