b489125a67131e6024d5222ac7c852ea5d3978b3
“Patrick <pctennislessons@gmail.com>
2020-04-08 16:03:25 -0600

    tests fail

--- maxSubArray.test.js
+++ maxSubArray.test.js
@@ -22,11 +22,16 @@ describe('maxSumArray', function() {
     describe('#maxSumArray', function(){
         
         it('returns an integer', function(){
-            
+            assert.equal(Number.isInteger(maxSumArray([-2,1,-3,4,-1,2,1,-5,4])), true)  
         })
 
         it('returns the highest possible sum for a contigous subarray.', function(){
-
+            assert.equal(maxSumArray([1,2,-1]), 3)
+            assert.equal(maxSumArray([1]), 1)
+            assert.equal(maxSumArray([1,2]), 3)
+            assert.equal(maxSumArray([1,2,3,4]), 10)
+            assert.equal(maxSumArray([-1,-2,-1,-4,-6]), -1)
+            assert.equal(maxSumArray([-2,1,-3,4,-1,2,1,-5,4]), 6)
         })
     })
 
